af6bef64981852bb770e476701d4d849bb9fcd77
bmax <bmax1337@gmail.com>
2015-01-17 20:15:09 -0500

    No PHP, also uses node js efficiently now.

--- nav.php
+++
@@ -1,7 +0,0 @@
-<div class="nav">
-        <a id="header" href="/"><h1>HackStream</h1></a>
-        <div id="navright">
-        	<a class="navlink" href="map.php">Map</a>
-        	<a class="navlink" href="">Login</a>
-    	</div>
-</div>
